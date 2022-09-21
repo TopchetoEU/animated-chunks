a10cc2e1b4b7edf983847a411cc1d3df0021e9c8
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2022-09-21 09:53:12 +0300

    feat: Add config manager

---
+++ src/main/java/me/topchetoeu/smoothchunks/ConfigManager.java
@@ -0,0 +1,11 @@
+package me.topchetoeu.smoothchunks;
+
+import java.io.File;
+
+public class ConfigManager {
+    public final File configFile;
+
+    public ConfigManager(File configFile) {
+        this.configFile = configFile;
+    }
+}
